486009d22f3357ccac9cf2bdf76626134b2353cd
Joseph Antony <jajoseph.antony18@gmail.com>
2024-02-18 09:35:27 +0530

    Casing:

--- server/index.js
+++ server/index.js
@@ -20,6 +20,6 @@ app.use("/*", (req, res) => {
 
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, dbName: "Courses" });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
